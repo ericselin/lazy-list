c54f977a95ad69d55b1095961c3cb042d3b5476d
Eric Selin <eric.selin@gmail.com>
2021-06-02 12:52:18 +0200

    feat: Create each and take functions
    Also refactoring and type fixes.

--- mod.test.ts
+++ mod.test.ts
@@ -7,30 +7,65 @@ function* abcdGenerator() {
   yield* abcdArray;
 }
 
-Deno.test("sequential running", async () => {
+function* numberGenerator() {
+  let i = 1;
+  while (true) {
+    yield i++;
+  }
+}
+
+const sleep = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
+
+Deno.test("run the pipeline one-by-one with `.sequential`", async () => {
   const result = await lazyList(abcdGenerator()).sequential();
   assertEquals(result, abcdArray);
 });
 
-Deno.test("map works with simple promises", async () => {
+Deno.test("map values using (potentially async) functions with `.map`", async () => {
   const result = await lazyList(abcdGenerator())
-    .map((str: string) => Promise.resolve(str + "a"))
-    .sequential();
-  assertEquals(result, ["aa", "ba", "ca", "da"]);
-});
-
-Deno.test("map works when chained", async () => {
-  const result = await lazyList(abcdGenerator())
-    .map((str: string) => Promise.resolve(str + "a"))
-    .map((str: string) => Promise.resolve(str + "b"))
-    .sequential();
-  assertEquals(result, ["aab", "bab", "cab", "dab"]);
-});
-
-Deno.test("map works with non-promises", async () => {
-  const result = await lazyList(abcdGenerator())
-    .map((str: string) => str + "a")
+    .map(async (str: string) => {
+      await sleep(10);
+      return str + "a";
+    })
     .map((str: string) => str + "b")
     .sequential();
   assertEquals(result, ["aab", "bab", "cab", "dab"]);
+});
+
+Deno.test("create (potentially async) side-effects with `.each`", async () => {
+  const sideEffects: string[] = [];
+  const asyncSideEffects: string[] = [];
+  const result: string[] = await lazyList(abcdGenerator())
+    .each((str: string) => {
+      sideEffects.push(str + "s");
+    })
+    .each(async (str: string) => {
+      await sleep(10);
+      asyncSideEffects.push(str + "a");
+    })
+    .sequential();
+  assertEquals(sideEffects, ["as", "bs", "cs", "ds"]);
+  assertEquals(asyncSideEffects, ["aa", "ba", "ca", "da"]);
+  assertEquals(result, ["a", "b", "c", "d"]);
+});
+
+Deno.test("side-effects with `.each` are created based on the last mapped value", async () => {
+  const sideEffects: string[] = [];
+  const result = await lazyList(abcdGenerator())
+    .map((str: string) => Promise.resolve(str + "a"))
+    .each(async (str: string) => {
+      await sleep(10);
+      sideEffects.push(str + "s");
+    })
+    .map((str: string) => Promise.resolve(str + "b"))
+    .sequential();
+  assertEquals(sideEffects, ["aas", "bas", "cas", "das"]);
+  assertEquals(result, ["aab", "bab", "cab", "dab"]);
+});
+
+Deno.test("limit the number of generated values with `.take`", async () => {
+  const results = await lazyList(numberGenerator())
+    .take(5)
+    .sequential();
+  assertEquals(results, [1, 2, 3, 4, 5]);
 });
--- mod.ts
+++ mod.ts
@@ -1,30 +1,53 @@
 type Unwrap<T> = T extends Promise<infer U> ? U : T;
 
-type LazyList = {
-  map: <F extends (arg: any) => any | Promise<any>>(
+type LazyList<A> = {
+  map: <F extends (arg: A) => unknown, R extends Unwrap<ReturnType<F>>>(
     fn: F,
-  ) => LazyList;
-  sequential: () => Promise<unknown[]>;
+  ) => LazyList<R>;
+  each: <F extends (arg: A) => unknown>(
+    fn: F,
+  ) => LazyList<A>;
+  take: (count: number) => LazyList<A>;
+  sequential: () => Promise<A[]>;
 };
 
-export const lazyList = (generator: Generator<unknown>): LazyList => {
-  let chainedGenerator: Generator = generator;
-  const self: LazyList = {
-    map: (fn) => {
-      // instead, just change generator[Symbol.asyncIterator]?
-      const gen = chainedGenerator;
-      chainedGenerator = function* () {
-        for (const promise of gen) {
-          if (promise instanceof Promise) yield promise.then(fn);
-          else yield fn(promise);
+export const lazyList = <A extends unknown>(
+  generator: Generator<A>,
+): LazyList<A> => {
+  const mapFns: ((arg: unknown) => unknown)[] = [];
+  const self: LazyList<A> = {
+    map: <F extends (arg: A) => unknown, R extends Unwrap<ReturnType<F>>>(
+      fn: F,
+    ) => {
+      mapFns.push(fn as (arg: unknown) => unknown);
+      return self as LazyList<R>;
+    },
+    each: <F extends (arg: A) => unknown>(fn: F) => {
+      mapFns.push(async (arg) => {
+        await fn(arg as A);
+        return arg;
+      });
+      return self as LazyList<A>;
+    },
+    take: (count) => {
+      const prevGenerator = generator;
+      generator = function* () {
+        for (let i = 0; i < count; i++) {
+          const { value, done } = prevGenerator.next();
+          if (done) return value;
+          yield value;
         }
       }();
       return self;
     },
     sequential: async () => {
-      const results: unknown[] = [];
-      for await (const element of chainedGenerator) {
-        results.push(element);
+      const results: A[] = [];
+      for await (const element of generator) {
+        const value = await mapFns.reduce(
+          (chain, fn) => chain.then(fn),
+          Promise.resolve(element as unknown),
+        );
+        results.push(value as A);
       }
       return results;
     },
